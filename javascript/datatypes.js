c369c6a81ae5844694f0d38ba2299180d301122c
vikasthakurr <vikaskumar20012001@gmail.com>
2025-08-29 13:21:23 +0530

    variables, datatypes

---
+++ javascript/datatypes.js
@@ -0,0 +1,30 @@
+var a = 10.5;
+console.log(typeof a);
+
+let name = "vikas";
+console.log(typeof name);
+
+let isLoggedIn = false;
+console.log(typeof isLoggedIn);
+
+//refrence type of data
+
+let person = {
+  name: "vikas",
+  age: 30,
+  isEmployed: true,
+};
+console.log(typeof person);
+
+let arr1 = [1, 2, 3, 4, 5];
+console.log(typeof arr1);
+
+function print() {
+  console.log(typeof print);
+}
+print();
+
+
+//
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ javascript/script.js
@@ -0,0 +1,42 @@
+// var a = 10;
+
+// function double(num) {
+//   return num * 2;
+// }
+// var double =function (num) {
+//   return num * 2;
+// }
+
+// var vikas = (num) => {
+//   return num * 2;
+// };
+// var res = vikas(6);
+// console.log(res);
+// var res1 = vikas(a);
+// console.log(res1);
+// const Myname = "vikas";
+
+// var a = 10;
+// let salary = 1234;
+// console.log(salary);
+
+// var position = "category Lead";
+// position= "mentor"
+// console.log(position);
+
+// console.log(Myname);
+// console.log(salary);
+
+// console.table({ Myname, salary, position });
+
+let a = 10;
+const b = 20;
+
+function print() {
+  console.log(a);
+  let c = 30;
+  console.log(c);
+}
+print();
+console.log(a);
+var c = 40;
