4cf325f6a391221c4fbd43bcef5b5d9aa7313229
vikasthakurr <vikaskumar20012001@gmail.com>
2025-09-09 12:40:56 +0530

    notes added

--- javascript/loops.js
+++ javascript/loops.js
@@ -1,26 +1,43 @@
+// In JavaScript, loops are used to repeat a block of code multiple times. 
+// This is useful for iterating over arrays, objects, or executing the same code for a specific number of times.
+
+// Let's start with an array that we will use for our loop examples.
 let arr1 = [1, 2, 3, 4, 5];
+
+// The traditional 'for' loop is the most common type of loop.
+// It has three parts: initialization, condition, and increment.
+// 1. Initialization: 'let i = 0' - This is where the loop starts. We declare a variable 'i' and set it to 0.
+// 2. Condition: 'i < arr1.length' - The loop will continue to run as long as this condition is true.
+// 3. Increment: 'i++' - This increases the value of 'i' by 1 after each iteration.
 // for (let i = 0; i < arr1.length; i++) {
 //   console.log(arr1[i]);
 // }
 
-//for of loop
+// The 'for...of' loop is a more modern and cleaner way to iterate over arrays.
+// It directly gives you the value of each element in the array.
+// 'i' here is not an index, but the actual value from the array (e.g., 1, 2, 3, etc.).
 // for (let i of arr1) {
 //   console.log(i);
 // }
 
-// for in loop
+// The 'for...in' loop is used to iterate over the properties of an object.
+// When used with an array, it gives you the 'index' of each element as a string.
+// It's generally not recommended to use 'for...in' to iterate over arrays because it can be slower and have unexpected behavior.
 // for (let i in arr1) {
 //   console.log(arr1[i]);
 // }
 
-//object iteration
+// Now let's see how to iterate over an object.
+// Objects store data in key-value pairs.
 let person = {
   name: "vikas",
   age: 25,
   isEmployed: true,
 };
 
-// for in loop
+// The 'for...in' loop is the standard way to iterate over the keys of an object.
+// 'key' will be "name", "age", and "isEmployed" in each iteration.
 // for (let key in person) {
+
 //   console.log(key + ": " + person[key]);
 // }
--- javascript/objects.js
+++ javascript/objects.js
@@ -1,3 +1,4 @@
+// Creating an object using the Object constructor (commented out example)
 // let person = new Object();
 // person.firstName = "John";
 // person.lastName = "Doe";
@@ -5,21 +6,24 @@
 // person.eyeColor = "blue";
 // console.log(person);
 
+// Creating an object using object literal syntax (commented out example)
 // let person = {
 //   firstName: "John",
 //   lastName: "Doe",
 //   age: 50,
 //   eyeColor: "blue",
-//   };
+// };
 
+// Assigning one object to another variable creates a reference, not a copy
 // let person1 = person;
 
-//shallow copy
-// let person1 ={...person}
+// Shallow copy using spread operator (copies top-level properties only)
+// let person1 = { ...person }
 // person1.age = 51;
 // console.log(person1);
 // console.log(person);
 
+// Creating a nested object with another object as a property
 let person = {
   firstName: "John",
   lastName: "Doe",
@@ -32,19 +36,38 @@ let person = {
   },
 };
 
+// Shallow copy using spread operator (does NOT deeply copy nested objects)
 // let person1 = { ...person };
 // person1.eyeColor = "green";
-// let person1 = structuredClone(person); //method1
-let person1 = JSON.stringify(person); //method2
-console.log(person1);
-console.log(typeof person1);
+
+// Deep copy using structuredClone (copies all nested objects/arrays)
+// let person1 = structuredClone(person); // method1
+
+// Deep copy using JSON methods (works if object contains only serializable data)
+let person1 = JSON.stringify(person); // method2: converts object to JSON string
+console.log(person1); // Outputs the JSON string representation of the object
+console.log(typeof person1); // Outputs 'string'
+
+// If you want to get the object back, use JSON.parse
+// let person1 = JSON.parse(JSON.stringify(person));
+
+// Changing a nested property after deep copy (uncomment to test)
 // person1.address.city = "Los Angeles";
 // console.log(person1);
 // console.log(person);
 
+// Primitive types (like strings) are copied by value, not by reference
 let name = "vikas";
 
-let name2 = name;
-name2 = "kumar";
-console.log(name2);
-console.log(name);
+let name2 = name; // name2 is a copy of name
+name2 = "kumar"; // changing name2 does not affect name
+console.log(name2); // Outputs: kumar
+console.log(name);  // Outputs: vikas
+
+/*
+  NOTES:
+  - Objects are assigned by reference, so changes to one affect the other unless you copy.
+  - Spread operator {...obj} creates a shallow copy (nested objects are still shared).
+  - structuredClone() and JSON methods can create deep copies, but JSON methods only work for serializable data (no functions, undefined, etc).
+  - Primitives (string, number, boolean, etc) are always copied by value.
+*/
